0e0336d5719a0345494e451af4577d6ca1699557
Gray Hound <admin@devilmaydie.name>
2012-11-27 17:06:10 +0400

    Added method for file operations and getting final url after operations and effects applied. example.php with examples and description (api requests and file operations)

--- uploadcare/lib/5.3-5.4/Uploadcare.php
+++ uploadcare/lib/5.3-5.4/Uploadcare.php
@@ -2,11 +2,16 @@
 namespace Uploadcare;
 
 define('API_TYPE_STORE', 'store');
+define('API_TYPE_RAW', 'raw');
+define('API_TYPE_ACCOUNT', 'account');
+define('API_TYPE_FILES', 'files');
 
 define('REQUEST_TYPE_POST', 'post');
 define('REQUEST_TYPE_PUT', 'put');
 define('REQUEST_TYPE_DELETE', 'delete');
 define('REQUEST_TYPE_GET', 'get');
+define('REQUEST_TYPE_HEAD', 'head');
+define('REQUEST_TYPE_OPTIONS', 'options');
 
 define('UC_PARAM_FILE_ID', 'file_id');
 
@@ -65,6 +70,22 @@ class Api
 	}	
 	
 	/**
+	 * Return an array of File objects to work with.
+	 * 
+	 * @return array
+	 **/
+	public function getFileList()
+	{
+		$data = $this->request(API_TYPE_FILES);
+		$files_raw = (array)$data->results;
+		$result = array();
+		foreach ($files_raw as $file_raw) {
+			$result[] = new File($file_raw->file_id, $this);
+		}
+		return $result;
+	}
+	
+	/**
 	 * Make request to API.
 	 * Throws Exception if not http code 200 was returned.
 	 * If http code 200 it will parse returned data form request as JSON.
@@ -75,7 +96,7 @@ class Api
 	 * @throws Exception
 	 * @return array
 	 **/
-	public function request($type, $request_type, $params = array())
+	public function request($type, $request_type = REQUEST_TYPE_GET, $params = array())
 	{
 		$ch = $this->__initRequest($type, $params);
 		$this->__setRequestType($ch, $request_type);
@@ -115,11 +136,17 @@ class Api
 	private function __getUrl($type, $params = array())
 	{
 		switch ($type) {
+			case API_TYPE_RAW:
+				return sprintf('https://%s/', $this->api_host);
+			case API_TYPE_ACCOUNT:
+				return sprintf('http://%s/account/', $this->api_host);
+			case API_TYPE_FILES:
+				return sprintf('http://%s/files/', $this->api_host);
 			case API_TYPE_STORE:
-				if (array_key_exists(UC_PARAM_FILE_ID, $params) == false)
-					throw new \Exception('Please provide "store_id" param for request');					
+				if (array_key_exists(UC_PARAM_FILE_ID, $params) == false) {
+					throw new \Exception('Please provide "store_id" param for request');			
+				}		
 				return sprintf('https://%s/files/%s/storage/', $this->api_host, $params['file_id']);
-				break;
 			default:
 				throw new \Exception('No api url type is provided for request. Use store, or appropriate constants.');
 		}
@@ -134,9 +161,11 @@ class Api
 	 * @throws Exception
 	 * @return void
 	 **/
-	private function __setRequestType($ch, $type)
+	private function __setRequestType($ch, $type = REQUEST_TYPE_GET)
 	{
 		switch ($type) {
+			case REQUEST_TYPE_GET:
+				break;			
 			case REQUEST_TYPE_POST:
 				curl_setopt($ch, CURLOPT_POST, true);
 				break;
@@ -146,8 +175,12 @@ class Api
 			case REQUEST_TYPE_DELETE:
 				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');			
 				break;
-			case REQUEST_TYPE_GET:
+			case REQUEST_TYPE_HEAD:
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'HEAD');
 				break;
+			case REQUEST_TYPE_OPTIONS:
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'OPTIONS');
+				break;				
 			default:
 				throw new \Exception('No request type is provided for request. Use post, put, delete, get or appropriate constants.');
 		}		
@@ -167,6 +200,8 @@ class Api
 			sprintf('Authorization: Uploadcare.Simple %s:%s', $this->public_key, $this->secret_key),
 			'Content-Type: application/json',
 			'Content-Length: 0',
+			'User-Agent: PHP Uploadcare Module',
+			sprintf('Date: %s', date('Y-m-d H:i:s')),
 		));		
 	}
 	
@@ -197,6 +232,13 @@ class File
 	 * @var string
 	 **/
 	private $file_id = null;
+	
+	/**
+	 * Operations and params for operations: crop, resize, scale_crop, effect.
+	 * 
+	 * @var array
+	 */
+	private $operations = array();
 
 	/**
 	 * Uploadcare class instance.
@@ -205,6 +247,11 @@ class File
 	 **/
 	private $api = null;
 
+	/**
+	 * Operations list
+	 **/
+	private $operation_list = array('crop', 'resize', 'scale_crop', 'effect');
+	
 	/**
 	 * Constructs an object for CDN file with specified ID
 	 *
@@ -215,6 +262,16 @@ class File
 	{
 		$this->file_id = $file_id;
 		$this->api = $api;
+	}
+	
+	/**
+	 * Return file_id for this file
+	 * 
+	 * @return string
+	 **/
+	public function getFileId()
+	{
+		return $this->file_id;
 	}
 
 	/**
@@ -234,18 +291,210 @@ class File
 	 **/
 	public function getUrl()
 	{
-		return sprintf('https://%s/%s/', $this->cdn_host, $this->file_id);
+		$url = sprintf('https://%s/%s/', $this->cdn_host, $this->file_id);
+		
+		$operations = array();
+		
+		foreach ($this->operations as $i => $operation_item) {
+			$part = array();
+			foreach (array_keys($operation_item) as $operation_type) {
+				$operation_params = $operation_item[$operation_type];
+				$part[] = $operation_type;	
+				switch ($operation_type) {
+					case 'crop':
+						$part = $this->__addPartSize($part, $operation_params);
+						$part = $this->__addPartCenter($part, $operation_params);
+						$part = $this->__addPartFillColor($part, $operation_params);
+						break;
+					case 'resize':
+						$part = $this->__addPartSize($part, $operation_params);
+						break;
+					case 'scale_crop':
+						$part = $this->__addPartSize($part, $operation_params);
+						$part = $this->__addPartCenter($part, $operation_params);
+						break;
+					case 'effect':
+						$part = $this->__addPartEffect($part, $operation_params);
+						break;
+				}
+				$part_str = join('/', $part);
+				$operations[] = $part_str;				
+			}
+		}
+		
+		if (count($operations)) {
+			$operations_part = join('/-/', $operations);
+			return $url.'-/'.$operations_part.'/';
+		} else {
+			return $url;
+		}
 	}	
+	
+	/**
+	 * Get object with cropped parameters.
+	 * 
+	 * @param integer $width Crop width
+	 * @param integer $height Crop height
+	 * @param boolean $center Center crop? true or false (default false).
+	 * @param string $fill_color Fill color. If nothig is provided just use false (default false).
+	 * @return File
+	 */
+	public function crop($width, $height, $center = false, $fill_color = false)
+	{
+		$result = clone $this;
+		$result->operations[]['crop'] = array(
+				'width' => $width,
+				'height' => $height,
+				'center' => $center,
+				'fill_color' => $fill_color,
+		);
+		return $result;
+	}
+	
+	/**
+	 * Get object with resized parameters.
+	 * Provide width or height or both.
+	 * If not width or height are provided exceptions will be thrown!
+	 * 
+	 * @param integer $width Resized image width. Provide false if you resize proportionally.
+	 * @param integer $height Resized image height. Provide false if you resize proportionally.
+	 * @throws \Exception
+	 * @return File
+	 **/
+	public function resize($width = false, $height = false)
+	{
+		$result = clone $this;
+		if (!$width && !$height) {
+			throw \Exception('Please, provide at least width or height for resize');
+		}
+		$result->operations[]['resize'] = array(
+				'width' => $width,
+				'height' => $height,
+		);
+		return $result;		
+	}
+	
+	/**
+	 * Get object with cropped parameters.
+	 *
+	 * @param integer $width Crop width
+	 * @param integer $height Crop height
+	 * @param boolean $center Center crop? true or false (default false).
+	 * @return File
+	 */
+	public function scaleCrop($width, $height, $center = false)
+	{
+		$result = clone $this;
+		$result->operations[]['scale_crop'] = array(
+				'width' => $width,
+				'height' => $height,
+				'center' => $center,
+		);
+		return $result;
+	}	
+	
+	/**
+	 * Apply flip effect
+	 * 
+	 * @return File
+	 **/
+	public function applyFlip()
+	{
+		$result = clone $this;
+		$result->operations[]['effect'] = 'flip';
+		return $result;
+	}
 
 	/**
-	 * Get 400x400 image
-	 * Test, will be removed
+	 * Apply grayscale effect
 	 *
-	 * @return string
+	 * @return File
 	 **/
-	public function getResizedUrl()
+	public function applyGrayscale()
 	{
-		return sprintf('%s-/crop/400x400/center', $this->getUrl());
+		$result = clone $this;
+		$result->operations[]['effect'] = 'grayscale';
+		return $result;
+	}
+
+	/**
+	 * Apply invert effect
+	 *
+	 * @return File
+	 **/
+	public function applyInvert()
+	{
+		$result = clone $this;
+		$result->operations[]['effect'] = 'invert';
+		return $result;
+	}	
+	
+	/**
+	 * Apply mirror effect
+	 *
+	 * @return File
+	 **/
+	public function applyMirror()
+	{
+		$result = clone $this;
+		$result->operations[]['effect'] = 'mirror';
+		return $result;
+	}	
+	
+	/**
+	 * Adds part with size for operations
+	 * 
+	 * @param array $part
+	 * @param array $params
+	 * @return array
+	 **/
+	private function __addPartSize($part, $params)
+	{
+		$part[] = sprintf('%sx%s', $params['width'], $params['height']);
+		return $part;
+	}
+	
+	/**
+	 * Adds part with center for operations
+	 *
+	 * @param array $part
+	 * @param array $params
+	 * @return array
+	 **/	
+	private function __addPartCenter($part, $params)
+	{
+		if ($params['center'] !== false) {
+			$part[] = 'center';
+		}		
+		return $part;
+	}
+	
+	/**
+	 * Adds part with fill color for operations
+	 *
+	 * @param array $part
+	 * @param array $params
+	 * @return array
+	 **/	
+	private function __addPartFillColor($part, $params)
+	{
+		if ($params['fill_color'] !== false) {
+			$part[] = $params['fill_color'];
+		}		
+		return $part;
+	}	
+	
+	/**
+	 * Adds part with effect for operations
+	 *
+	 * @param array $part
+	 * @param string $effect
+	 * @return array
+	 **/	
+	private function __addPartEffect($part, $effect)
+	{
+		$part[] = $effect;
+		return $part;
 	}
 }
 
