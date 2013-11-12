3cc562b056f06c4ddcaf1a7da8c580c229a34136
amosmos <amos@boaideas.com>
2013-11-12 15:33:42 +0200

    201 Status code is also good

--- uploadcare/lib/5.3-5.4/Api.php
+++ uploadcare/lib/5.3-5.4/Api.php
@@ -159,7 +159,7 @@ class Api
         throw new \Exception('Request returned unexpected http code '.$ch_info['http_code'].'. '.$data);
       }
     } else {
-      if ($ch_info['http_code'] != 200) {
+      if (($ch_info['http_code'] != 200)&&($ch_info['http_code'] != 201)) {
         throw new \Exception('Request returned unexpected http code '.$ch_info['http_code'].'. '.$data);
       }
     }
