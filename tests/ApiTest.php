0a09af40d2c1052e5c25fb905afaf6269f19d35b
Mark van Eijk <mark@vaneijk.co>
2019-08-28 23:12:33 +0200

    Created seperate tests for setting custom filename

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -385,6 +385,24 @@ class ApiTest extends TestCase
         } catch (Exception $e) {
             $this->fail('We get an unexpected exception trying to store uploaded file from path: '.$e->getMessage());
         }
+    }
+
+    /**
+     * Test uploading from path
+     */
+    public function testUploadFromPathWithCustomFilename()
+    {
+        try {
+            $file = $this->api->uploader->fromPath(dirname(__FILE__).'/test.jpg', 'image/jpeg', 'rename.jpg');
+        } catch (Exception $e) {
+            $this->fail('We get an unexpected exception trying to upload from path: '.$e->getMessage());
+        }
+
+        try {
+            $file->store();
+        } catch (Exception $e) {
+            $this->fail('We get an unexpected exception trying to store uploaded file from path: '.$e->getMessage());
+        }
 
         usleep(2000000); // wait 2 sec to give a time to prepare file and avoid 404 error.
 
@@ -421,10 +439,26 @@ class ApiTest extends TestCase
         }
 
         usleep(2000000); // wait 2 sec to give a time to prepare file and avoid 404 error.
-        $url = $file->getUrl();
-        $text = file_get_contents($url);
+        $text = file_get_contents($file->getUrl());
 
         $this->assertEquals($text, "This is some text I want to upload");
+    }
+
+    public function testUploadFromStringWithCustomFilename()
+    {
+        try {
+            $content = "This is some text I want to upload";
+            $file = $this->api->uploader->fromContent($content, 'text/plain', 'test.txt');
+        } catch (Exception $e) {
+            $this->fail('We get an unexpected exception trying to upload from contents: '.$e->getMessage());
+        }
+        try {
+            $file->store();
+        } catch (Exception $e) {
+            $this->fail('We get an unexpected exception trying to store uploaded file from contents: '.$e->getMessage());
+        }
+
+        usleep(2000000);
 
         $this->assertEquals($file->data['original_filename'], "test.txt");
     }
