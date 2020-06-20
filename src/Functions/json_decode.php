a1a103d819c2a34c4f6fe87bd6b3fc42d0e6ff82
Andrew Zhdanovskih <azh@crtweb.ru>
2020-06-20 14:21:42 +0500

    Fix json_encode for 5.3

--- src/Functions/json_decode.php
+++ src/Functions/json_decode.php
@@ -6,6 +6,10 @@ use Uploadcare\Exceptions\JsonException;
 
 function jsonDecode($json, $assoc = false, $depth = 512, $options = 0)
 {
+    if (PHP_MAJOR_VERSION === 5 && PHP_MINOR_VERSION <= 3) {
+        return \json_decode($json, $assoc, $options);
+    }
+
     $result = \json_decode($json, $assoc, $depth, $options);
     if (json_last_error() === JSON_ERROR_NONE) {
         return $result;
