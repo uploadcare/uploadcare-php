543794543241cfe35fcc517b565309c3ddfdb240
Andrew Zhdanovskikh <andrew72ru@gmail.com>
2023-02-04 11:25:57 +0100

    Update webhook put

--- api-samples/rest-api/put-webhooks-id.php
+++ api-samples/rest-api/put-webhooks-id.php
@@ -2,6 +2,11 @@
 
 $configuration = Uploadcare\Configuration::create((string) $_ENV['UPLOADCARE_PUBLIC_KEY'], (string) $_ENV['UPLOADCARE_SECRET_KEY']);
 $api = (new Uploadcare\Api($configuration))->webhook();
-$webhook = $api->updateWebhook(1234, ['https://yourwebhook.com', true]);
+$webhook = $api->updateWebhook(1234, [
+    'target_url' => 'https://yourwebhook.com',
+    'event' => 'file.uploaded',
+    'is_active' => true,
+    'signing_secret' => 'webhook-secret',
+]);
 
 \sprintf("Webhook with url %s is %s\n", $webhook->getTargetUrl(), $webhook->isActive() ? 'active' : 'not active');
